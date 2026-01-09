60a82e0ea27291843edc7e21026b66aa1e596073
Hanice Sun <sunhanice@gmail.com>
2026-01-08 19:21:05 -0800

    hanice

--- src/deepSec/preprocess.py
+++ src/deepSec/preprocess.py
@@ -17,9 +17,10 @@ class DataProcessor():
         S = {}
         for f in in_files:
             if f.find('abinitio') == -1:
-                k = f.replace('.cdna.', '.').replace('.cds.', '.').replace('.pep.', '.')
-                S.setdefault(k, [])
-                S[k].append(f)
+                if k.find('.cdna.') != -1 or f.find('.cds.') != -1 or f.find('.pep.') != -1:
+                    k = f.replace('.cdna.', '.').replace('.cds.', '.').replace('.pep.', '.')
+                    S.setdefault(k, [])
+                    S[k].append(f)
 
         for k in sorted(S):
             if len(S[k]) == 3:
